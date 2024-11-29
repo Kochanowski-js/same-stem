3bd8a3f85c23d07a6b5753c649586fa3007614f0
Mona Lisa <roxmb25@gmail.com>
2024-11-29 17:28:49 +0100

    Resolve giant memory usage; increment version to 1.0.4

--- index.ts
+++ index.ts
@@ -2,17 +2,35 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 let cachedFileContent: string | null = null;
+let cachedParsedContent: { [key: string]: string } | null = null;
 
 function readFileContent(): string {
   if (cachedFileContent === null) {
-    const filePath = path.join(__dirname, "../data/sgjp-20241117-lemmatized.tab");
+    const filePath = path.join(__dirname, "sgjp-20241117-lemmatized.tab");
     cachedFileContent = fs.readFileSync(filePath, 'utf-8');
   }
   return cachedFileContent;
 }
 
+function parseFileContent(): { [key: string]: string } {
+  if (cachedParsedContent === null) {
+    const fileContent = readFileContent();
+    const lines = fileContent.split('\n');
+    cachedParsedContent = {};
+
+    for (const line of lines) {
+      const [baseForm, ...forms] = line.split('\t');
+      for (const form of forms) {
+        cachedParsedContent[form] = baseForm;
+      }
+    }
+  }
+  return cachedParsedContent;
+}
+
+const parsedContent = parseFileContent();
+
 export default function sameStem(word1: string, word2: string): boolean {
-  
   if (word1 === word2) {
     return true;
   }
@@ -21,61 +39,39 @@ export default function sameStem(word1: string, word2: string): boolean {
     return true;
   }
 
-  const stem1 = getStem(word1);
-  const stem2 = getStem(word2);
+  const stem1 = parsedContent[word1];
+  const stem2 = parsedContent[word2];
 
   if (stem1 !== undefined && stem2 !== undefined) {
     return stem1 === stem2;
   }
 
   return levenshtein(word1, word2) <= Math.floor(Math.min(word1.length, word2.length) / 3);
-
-}
-
-/**
- * @param word Input word, a string
- * @returns Base form of the word
- */
-function getStem(word: string): string | undefined {
-  const fileContent = readFileContent();
-  const lines = fileContent.split('\n');
-
-  for (const line of lines) {
-    const [baseForm, ...forms] = line.split('\t');
-    if (forms.includes(word)) {
-      return baseForm;
-    }
-  }
-
-  return undefined;
 }
 
 function levenshtein(a: string, b: string): number {
   const matrix: number[][] = [];
 
-  // increment along the first column of each row
   for (let i = 0; i <= b.length; i++) {
-      matrix[i] = [i];
+    matrix[i] = [i];
   }
 
-  // increment each column in the first row
   for (let j = 0; j <= a.length; j++) {
-      matrix[0][j] = j;
+    matrix[0][j] = j;
   }
 
-  // fill in the rest of the matrix
   for (let i = 1; i <= b.length; i++) {
-      for (let j = 1; j <= a.length; j++) {
-          if (b.charAt(i - 1) === a.charAt(j - 1)) {
-              matrix[i][j] = matrix[i - 1][j - 1];
-          } else {
-              matrix[i][j] = Math.min(
-                  matrix[i - 1][j - 1] + 1, // substitution
-                  matrix[i][j - 1] + 1,     // insertion
-                  matrix[i - 1][j] + 1      // deletion
-              );
-          }
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          matrix[i][j - 1] + 1,     // insertion
+          matrix[i - 1][j] + 1      // deletion
+        );
       }
+    }
   }
 
   return matrix[b.length][a.length];
